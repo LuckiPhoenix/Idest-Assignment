13677b8f2f71755774e1588adec914c8f3a1715c
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-11-19 11:43:53 +0700

    chore(assignment): enhance findAll method to aggregate results from reading, listening, writing, and speaking services

--- src/assignment/assignment.service.ts
+++ src/assignment/assignment.service.ts
@@ -2,16 +2,36 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Assignment, AssignmentDocument } from './schemas/assignment.schema';
+import { ReadingService } from './reading/reading.service';
+import { ListeningService } from './listening/listening.service';
+import { WritingService } from './writing/writing.service';
+import { SpeakingService } from './speaking/speaking.service';
 
 @Injectable()
 export class AssignmentService {
   constructor(
     @InjectModel(Assignment.name)
     private assignmentModel: Model<AssignmentDocument>,
+    private readonly readingService: ReadingService,
+    private readonly listeningService: ListeningService,
+    private readonly writingService: WritingService,
+    private readonly speakingService: SpeakingService,
   ) {}
 
   async findAll() {
-    return this.assignmentModel.find().exec();
+    const [reading, listening, writing, speaking] = await Promise.all([
+      this.readingService.findAll(),
+      this.listeningService.findAll(),
+      this.writingService.findAll(),
+      this.speakingService.findAll(),
+    ]);
+
+    return {
+      reading,
+      listening,
+      writing,
+      speaking,
+    };
   }
 
   async findOne(id: string) {
